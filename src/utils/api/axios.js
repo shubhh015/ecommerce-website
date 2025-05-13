b5a96bb2093820ab2c037fc8e0396ea6a7cc6bcd
shubhh015 <shubhashishhhh@gmail.com>
2025-05-13 14:16:06 +0530

    changed base url

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://ecommerce-backend-n66a.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
